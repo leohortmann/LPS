8e1f934c0eb6a3e1d17ff75ccc9fb96a52cc6eef
leohortmann <110673114+leohortmann@users.noreply.github.com>
2022-08-05 11:50:15 -0300

    Create L2Q11.c
    Faça um programa que apresente (imprima com %x os resultados das expressões):
    a) O valor hexadecimal do resultado do operador ~0x55
    b) O valor hexadecimal do resultado da operação 0x09 << 2
    c) O valor hexadecimal do resultado da operação 0x09 >> 2
    d) O valor hexadecimal do resultado da operação 0xFF >> 2
    e) O valor hexadecimal do resultado da operação 0xFF << 2

---
+++ Lista2/L2Q11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("a) %x\n", ~0x55);
+    printf("b) %x\n", 0x09<<2);
+    printf("c) %x\n", 0x09>>2);
+    printf("d) %x\n", 0xFF>>2);
+    printf("e) %x", 0xFF<<2);
+        
+
+
+
+
+}
