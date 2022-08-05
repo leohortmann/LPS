48fdffa6ff82a55753f19072f0795228cdc82159
leohortmann <110673114+leohortmann@users.noreply.github.com>
2022-08-05 11:30:38 -0300

    Create L2Q3.c
    Faça um programa que leia 10 números inteiros e apresente o maior (não utilize vetor/matriz).

---
+++ Lista2/L2Q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int entrada, maior, i;
+
+    scanf("%d", &maior);
+
+    for (i=0;i<=8;i++) {
+        scanf("%d", &entrada);
+
+        if (entrada>maior) {
+            maior = entrada;
+            entrada = 0;
+        } else {
+            entrada = 0;
+        }
+    }
+
+    printf("%d",maior);
+
+    return 0;
+
+}
