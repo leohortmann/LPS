9778dd3ffce3875d050b8045a83631a0311f19b9
leohortmann <110673114+leohortmann@users.noreply.github.com>
2022-08-05 11:47:53 -0300

    Create L2Q9.c
    Faça uma função isBit que receba dois parâmetros (nro e bit), a função deve retornar FALSO (0) se o n-bit estiver com valor 0 ou, no caso contrário, retornar VERDADEIRO.

---
+++ Lista2/L2Q9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int isBit(int nro, int bit)
+{
+    int mask=1<<bit;
+
+    mask=mask&bit;
+
+    if (mask>=bit){
+        return 1;
+    } else {
+        return 0;
+    }
+
+
+}
+
+int main(void) {
+    int num,bit;
+    scanf("%d %d",&num,&bit);
+    printf("%d\n", isBit(num,bit) );
+}
